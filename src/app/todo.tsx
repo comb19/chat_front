b123085730c8d8e7305ec80f08dde6bedac8712e
keito0tada <keito0tada@gmail.com>
2025-04-17 00:39:00 +0900

    add createtodo

---
+++ src/app/todo.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState } from "react";
+
+  
+
+export default function CreateTodo() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const body = JSON.stringify({ title, description });
+        console.log(body);
+        const res = await fetch('http://localhost:8080/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: body,
+        });
+
+        console.log(res);
+    }
+
+    return (
+        <form onSubmit={onSubmitHandler} className="flex flex-col items-center justify-between ">
+            <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+            <input type="text" value={description} onChange={e => setDescription(e.target.value)}/>
+            <button type="submit">Create Todo</button>
+        </form>
+    )
+}
